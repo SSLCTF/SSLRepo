a98d1805c51e7049605e12432c1c31a0363ff880
SSLCTF <7rs5tsywmf@privaterelay.appleid.com>
2026-03-26 00:30:05 +0700

    Include v1 to main router, set uvicorn reload to false

--- core/api/api.py
+++ core/api/api.py
@@ -5,3 +5,4 @@ from core.api.v1 import v1
 
 router = APIRouter(prefix="/api")
 
+router.include_router(v1.router)
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from core import environment
 environment.load()
 
 if __name__ == "__main__":
-    uvicorn.run("core.app:app", host="0.0.0.0", port=8000, reload=True) # TODO: False
+    uvicorn.run("core.app:app", host="0.0.0.0", port=8000, reload=False)
