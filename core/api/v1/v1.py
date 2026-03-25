7bcece1edf9afc13a100673a4d704eb3f60c53cb
SSLCTF <7rs5tsywmf@privaterelay.appleid.com>
2026-03-26 00:28:50 +0700

    Update .gitidnore, fix ctf_policy, include flag router to v1

--- core/api/v1/v1.py
+++ core/api/v1/v1.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
-from fastapi.security import HTTPBearer
+from fastapi import APIRouter, Depends
+
+from core.api.v1 import flag
+from dependency.auth import ctf_policy
 
 
 router = APIRouter(prefix="/v1")
+
+router.include_router(flag.router, dependencies=[Depends(ctf_policy)])
